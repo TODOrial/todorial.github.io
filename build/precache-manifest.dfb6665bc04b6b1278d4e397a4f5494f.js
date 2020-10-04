ba2cad12b858bc3115ae18ad6b1b334b9a7eef24
Bruno Silva <bruno3dcontato@gmail.com>
2020-10-04 00:11:55 -0300

    fix: changed break-all to break-word

---
+++ build/precache-manifest.dfb6665bc04b6b1278d4e397a4f5494f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8818684e208838df9dd678c174f64cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b03b71faa26a1fb1305",
+    "url": "/static/css/main.c1563ea8.chunk.css"
+  },
+  {
+    "revision": "5519dbbc214da2bb80d0",
+    "url": "/static/js/2.298b172c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.298b172c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b03b71faa26a1fb1305",
+    "url": "/static/js/main.63e9d10e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
