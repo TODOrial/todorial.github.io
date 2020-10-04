c693691a509fa05e3e016cf9585bd0acbd2aa306
Bruno Silva <bruno3dcontato@gmail.com>
2020-10-03 22:26:18 -0300

    Updates

---
+++ precache-manifest.ac84d18573e6d98fbe0534d87fd5a3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdffc38ae827ae4d5af49fb8127f47de",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "1edc74455eaeab6e5c88",
+    "url": "/frontend/static/css/main.7cac8ad0.chunk.css"
+  },
+  {
+    "revision": "5519dbbc214da2bb80d0",
+    "url": "/frontend/static/js/2.298b172c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend/static/js/2.298b172c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1edc74455eaeab6e5c88",
+    "url": "/frontend/static/js/main.6567690a.chunk.js"
+  },
+  {
+    "revision": "1feb07dcc0e8ad410fa7",
+    "url": "/frontend/static/js/runtime-main.1c82cd70.js"
+  }
+]);
